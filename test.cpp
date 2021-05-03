594ca5fa6ab3b27117f05c1fdc9b2e85499093af
loriange <79505596+loriange@users.noreply.github.com>
2021-05-03 18:14:42 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,12 +8,14 @@ class Test_programma_computer : public testing::Test
 {
 	public:
 		Computer* c, * c1, * c2;
+		Magazzino* m;
 
 	virtual void SetUp()
 	{
 		c = new Computer();
 		c1 = new Computer("Asus", "VivoBook", 2, 8, 400, 50, 2016, 1);
-		c2 = new Computer("Asus", "VivoBook", 3, 16, 1000, 60, 2019, 2);	
+		c2 = new Computer("Asus", "VivoBook", 3, 16, 1000, 60, 2019, 2);
+		m = new Magazzino();
 	}
 
 	virtual void TearDown()
@@ -21,6 +23,7 @@ class Test_programma_computer : public testing::Test
 		delete c;
 		delete c1;
 		delete c2;
+		delete m;
 	}
 };
 
