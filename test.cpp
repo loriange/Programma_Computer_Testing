2006c6840a4b52d6060bdac2c8b3f1b64adc9162
federicotagliaferri <79504724+federicotagliaferri@users.noreply.github.com>
2021-05-04 08:16:32 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -33,6 +33,60 @@ class Test_programma_computer : public testing::Test
 		delete m;
 	}
 };
+    
+//test del costruttore con parametri da input della classe Computer(parametri giusti)
+TEST(Computer, costruttore_con_parametri_giusti) 
+{
+    Computer c1("Asus", "VivoBook", 2, 8, 400, 50, 2016, 1);
+    ASSERT_TRUE(c1.getMarca() == "Asus");
+    ASSERT_TRUE(c1.getModello() == "VivoBook");
+    ASSERT_TRUE(c1.getVelocita() == 2);
+    ASSERT_TRUE(c1.getRam() == 8);
+    ASSERT_TRUE(c1.getDisco() == 400);
+    ASSERT_TRUE(c1.getMonitor() == 50);
+    ASSERT_TRUE(c1.getAnno() == 2016);
+    ASSERT_TRUE(c1.getCodice() == 1);
+}
+
+
+//test del costruttore con parametri da input della classe Computer(parametri sbagliati)
+TEST(Computer, costruttore_con_parametri_sbagliati)
+{
+    Computer c1("Asus", "VivoBook", 2, 8, 400, 50, 2016, 1);
+
+    ASSERT_FALSE(c1.getMarca() == "bo");
+    ASSERT_FALSE(c1.getModello() == "bo");
+    ASSERT_FALSE(c1.getVelocita() == 10);
+    ASSERT_FALSE(c1.getRam() == 10);
+    ASSERT_FALSE(c1.getDisco() == 10);
+    ASSERT_FALSE(c1.getMonitor() == 10);
+    ASSERT_FALSE(c1.getAnno() == 10);
+    ASSERT_FALSE(c1.getCodice() == 10);
+}
+ 
+//test della funzione ricerca pc della classe Magazzino
+TEST(Magazzino, ricerca_pc)
+{
+    Magazzino m;
+
+    Computer c1("Asus", "VivoBook", 2, 8, 400, 50, 2016, 1);
+    Computer c2("Asus", "VivoBook", 3, 16, 1000, 60, 2019, 2);
+
+    ASSERT_TRUE(m.aggiungi(c1, 1));
+    ASSERT_TRUE(m.aggiungi(c2, 2));
+
+    ASSERT_TRUE(m.ricercaCodice(1));
+
+    ASSERT_FALSE(m.ricercaCodice(5));
+    ASSERT_FALSE(m.ricercaCodice(9));
+}
+ 
+
+
+
+
+
+
 
 
 
